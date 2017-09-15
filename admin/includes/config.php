7d032296881fed8b65f2efd0cabc384c38f4d02f
root <christian.hernandez@yahoo.com>
2017-09-15 16:10:26 -0700

    arrrggghhhhh fixed!

---
+++ admin/includes/config.php
@@ -0,0 +1,13 @@
+<?php
+// Define Database config
+defined("DB_HOST") ? null : define("DB_HOST", getenv("MYSQL_SERVICE_HOST"));
+defined("DB_USER") ? null : define("DB_USER", getenv("MYSQL_USERNAME"));
+defined("DB_PASS") ? null : define("DB_PASS", getenv("MYSQL_PASSWORD"));
+defined("DB_NAME") ? null : define("DB_NAME", getenv("MYSQL_DATABASE"));
+
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (!$con) {
+	echo "Database error: " . mysqli_error($con);
+}
+?>
---
+++ admin/users.php
@@ -0,0 +1,43 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+            <!-- Topbar Menu Items -->
+		<?php include("includes/top_nav.php"); ?>
+
+            <!-- Sidebar Menu Items -->
+		<?php include("includes/side_nav.php"); ?>
+
+        </nav>
+        <!-- /Navigation -->
+
+        <!-- /#page-wrapper -->
+        <div id="page-wrapper">
+	    <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Users
+                            <small>Subheading</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-users"></i> Users
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
